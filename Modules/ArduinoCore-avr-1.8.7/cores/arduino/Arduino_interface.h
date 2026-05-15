68f0601b9ad26186da278076025f3a3c6504d93a
ZHANG-Qidi <grunt4991@gmail.com>
2026-05-15 23:48:58 +0900

    更新 README

--- Modules/ArduinoCore-avr-1.8.7/cores/arduino/Arduino_interface.h
+++ Modules/ArduinoCore-avr-1.8.7/cores/arduino/Arduino_interface.h
@@ -15,7 +15,6 @@ extern "C" {
 #define PIN_MOSI 19
 
 // I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define I2C_PORT i2c0
 #define I2C_SDA 4
